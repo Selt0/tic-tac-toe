b0a60897c31e1a04d4e3378fb9cd9fc1bd2d6b54
Selt0 <chewedgum12@gmail.com>
2020-05-05 12:16:27 -0500

    Add functionality to computer class

--- v3/computer.rb
+++ v3/computer.rb
@@ -1,5 +1,13 @@
 class Computer
-  def initialize
-    
+attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def get_pos(legal_positions)
+    choice = legal_positions.sample
+    puts "Computer #{mark} chose position #{choice.join(' ')}"
+    choice
   end
 end
