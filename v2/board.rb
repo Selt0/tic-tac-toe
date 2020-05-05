d2d413d7f39bb5919a42f4279656e883554edc46
Selt0 <chewedgum12@gmail.com>
2020-05-05 10:24:41 -0500

    Restyle grid to support dynamic grid

--- v2/board.rb
+++ v2/board.rb
@@ -2,8 +2,8 @@ class Board
 
   attr_reader :grid
 
-  def initialize
-    @grid = Array.new(3) {Array.new(3, '_') }
+  def initialize(size)
+    @grid = Array.new(size) {Array.new(size, '_') }
   end
 
   def [](pos)
@@ -33,21 +33,9 @@ class Board
   end
 
   def print
-    pretty_board = grid.map do |row|
-      row.map.with_index do |mark, i|
-        if i == grid.length / 2
-         " | #{mark.to_s} | "
-        else
-          mark
-        end
-      end
-    end
-    puts "  #{(0...grid.length).to_a.join('   ')}"
-
-    pretty_board.each_with_index do |row, i|
-      puts "#{i} #{row.join('')}"
-      break if i == grid.length - 1
-      puts "  #{'-'*grid.length ** 2}"
+    puts "  #{(0...grid.length).to_a.join(' ')}"
+    grid.each_with_index do |row, i|
+      puts "#{i} #{row.join(' ')}"
     end
   end
   
