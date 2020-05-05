a611ea712bddb34d9c1338f3d1796b0e47d66ad9
Selt0 <chewedgum12@gmail.com>
2020-05-05 12:17:26 -0500

    restructure game file to include computer player

--- v3/game.rb
+++ v3/game.rb
@@ -1,26 +1,27 @@
 require_relative 'board'
 require_relative 'human_player'
+require_relative 'computer'
 
 class Game
 
   attr_reader :players, :current_player, 
               :board
 
-  def initialize(size, *marks)
+  def initialize(size, marks = {})
     @board = Board.new(size)
-    @players = marks.map{ |mark| Player.new(mark) }
+    @players = marks.map{ |mark, comp| comp ? Computer.new(mark) :  Player.new(mark) }
     @current_player = players.first
   end
 
   def run
     until game_over?
-      #print the board
       puts
       board.print
       puts
-      #get position from player
-      #mark the board
-      board.place_mark(current_player.mark, current_player.get_pos)
+      choices = board.legal_positions
+      player_pos = current_player.get_pos(choices)
+
+      board.place_mark(current_player.mark, player_pos)
 
       if board.win?(current_player.mark)
         board.print
@@ -31,7 +32,7 @@ class Game
     end
     board.print
     puts
-    puts "DRAW!"
+    return "DRAW!"
   end
 
   def switch_turns
