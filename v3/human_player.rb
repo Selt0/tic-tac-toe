9629c75f578f8122254c0203112744068f9ee0bb
Selt0 <chewedgum12@gmail.com>
2020-05-05 11:02:14 -0500

    Init v3 (computer player)

---
+++ v3/human_player.rb
@@ -0,0 +1,34 @@
+class Player
+
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def get_pos
+    pos = nil
+
+    until pos
+      puts "Player #{mark}, enter a position on the board (e.g., 0 1)"
+      print "> "
+      begin
+        pos = parse_pos(gets.chomp)  
+        raise  if !valid_pos?(pos)
+      rescue => exception
+        puts "Invalid position. (Make sure you use a space!)"
+        puts
+        pos = nil
+      end
+    end
+    pos
+  end
+
+  def valid_pos?(pos)
+    pos.length == 2 && pos.is_a?(Array)
+  end
+
+  def parse_pos(pos)
+    pos.split.map(&:to_i)
+  end
+end
