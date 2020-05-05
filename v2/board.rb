9679c593b49df221b585b9196fab611c049c6226
Selt0 <chewedgum12@gmail.com>
2020-05-05 10:23:55 -0500

    Init v2.

---
+++ v2/board.rb
@@ -0,0 +1,77 @@
+class Board
+
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(3) {Array.new(3, '_') }
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, mark)
+    x, y = pos
+    grid[x][y] = mark
+  end
+
+  def valid?(pos)
+    pos.all?{ |ele| ele.between?(0, grid.length)}
+  end
+
+  def empty?(pos)
+    self[pos] == '_'
+  end
+
+  def place_mark(mark, pos)
+    if valid?(pos) && empty?(pos)
+      self[pos] = mark 
+    else   
+      raise "Invalid position"
+    end
+  end
+
+  def print
+    pretty_board = grid.map do |row|
+      row.map.with_index do |mark, i|
+        if i == grid.length / 2
+         " | #{mark.to_s} | "
+        else
+          mark
+        end
+      end
+    end
+    puts "  #{(0...grid.length).to_a.join('   ')}"
+
+    pretty_board.each_with_index do |row, i|
+      puts "#{i} #{row.join('')}"
+      break if i == grid.length - 1
+      puts "  #{'-'*grid.length ** 2}"
+    end
+  end
+  
+  def win_row?(mark)
+    grid.any?{|row| row.all?(mark)}
+  end
+
+  def win_col?(mark)
+    grid.transpose.any?{ |col| col.all?(mark)}
+  end
+
+  def win_diagonal?(mark)
+    left_to_right = (0...grid.length).collect { |i| [i,i] }
+    right_to_left = (0...grid.length).collect { |i| [i, -i - 1] }
+    
+    left_to_right.all?{ |pos| self[pos] == mark} ||
+    right_to_left.all?{ |pos| self[pos] == mark}
+  end
+
+  def win?(mark)
+    win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+  end
+
+  def empty_positions?
+    grid.flatten.any?{ |pos| pos == '_' }
+  end
+end
---
+++ v2/game.rb
@@ -0,0 +1,44 @@
+require_relative 'board'
+require_relative 'human_player'
+
+class Game
+
+  attr_reader :players, :current_player, 
+              :board
+
+  def initialize(player1_mark, player2_mark)
+    @board = Board.new
+    @players = [Player.new(player1_mark), Player.new(player2_mark)]
+    @current_player = players.first
+  end
+
+  def run
+    until game_over?
+      #print the board
+      puts
+      board.print
+      puts
+      #get position from player
+      #mark the board
+      board.place_mark(current_player.mark, current_player.get_pos)
+
+      if board.win?(current_player.mark)
+        board.print
+        return "Player #{current_player.mark} has won the game!"
+      else
+        switch_turns
+      end
+    end
+    board.print
+    puts
+    puts "DRAW!"
+  end
+
+  def switch_turns
+    @current_player = players.rotate!.first
+  end
+
+  def game_over?
+    !board.empty_positions?
+  end
+end
